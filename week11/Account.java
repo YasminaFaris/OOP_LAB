11828d858579fb5e0d68c87289b1ee5c5186a5cb
YasminaFaris <yah2nasser@gmail.com>
2025-03-07 12:45:05 +0700

    Update Account.java

--- week11/Account.java
+++ week11/Account.java
@@ -3,42 +3,53 @@ public class Account {
     protected double balance;
     protected String name;
     
-    public Account(double balance, String name){
+    public Account(double balance , String name){
         this.balance = balance;
         this.name = name;
+        
     }
-    public void setName(String name){
-        this.name = name;
-    }
-    public String getName(){
-        return name;
-    }
-    public void setBalance(double balance){
-        this.balance = balance;
-    }
-    public double getBalance(){
+    public double getBalance() {
         return balance;
     }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
     public void deposit(double a){
-        if (a > 0){
+        if (a >= 0){
             this.setBalance(this.getBalance() + a);
-            System.out.println(a+" baht is deposited to "+this.getName()+".");
+            System.out.println(a+" baht is deposited to "+this.getName() + ".");
+        }
+        else{
+            System.out.println("Input number must be a positive integer.");
         }
     }
-    public void withdraw(double amount) throws WithdrawException{
-        if (amount >= 0){
-            if (this.getBalance() - amount >= 0){
-                this.setBalance(this.getBalance() - amount);
-                System.out.println(amount+" baht is withdrawn from "+this.getName()+".");
-            }else{
-                throw new WithdrawException("Not enough money!");
-            }   
-        }else{
-            throw new WithdrawException("Input number must be a positive integer.");
+    
+    public void withdraw(double amount) throws WithdrawException {
+        if (amount >= 0) {
+            balance -= amount;
+            System.out.println(amount + " baht is withdrawn from " + name + ".");
+        }
+        else {
+            if (amount < 0) {
+                throw new WithdrawException("Account " + name + " has not enough money.!");
+            }
+            else if ((balance - amount) < 0) {
+                throw new WithdrawException("Account " + name + " has not enough money.!");
+            }
         }
     }
+    
     public void showAccount(){
         System.out.println(this.name+" account has "+this.balance+" baht.");
     }
-    
 }

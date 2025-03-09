913e64806d7051c430181cb5e48c99a2dcc5ed5a
YasminaFaris <yah2nasser@gmail.com>
2025-03-09 13:22:16 +0700

    Update WithdrawException.java

--- week11/WithdrawException.java
+++ week11/WithdrawException.java
@@ -11,7 +11,7 @@ public class WithdrawException extends Exception{
     public WithdrawException(){
         super();
     }
-    public WithdrawException(String message){
-        super(message);
+    public WithdrawException(String s){
+        super(s);
     }
 }
